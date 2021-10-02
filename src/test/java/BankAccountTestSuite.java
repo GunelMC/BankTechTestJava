fa068b54f95c3af03bab2782eff9ec8f4575d05e
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-10-02 17:37:15 +0100

    Refactor the tests: use Mockito in place of manually created mocks

--- src/test/java/BankAccountTestSuite.java
+++ src/test/java/BankAccountTestSuite.java
@@ -5,82 +5,35 @@ import org.mockito.Mockito;
 import java.time.LocalDate;
 import java.util.Arrays;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.*;
 
 public class BankAccountTestSuite {
-    private class MockTransaction implements Transaction {
 
-        @Override
-        public Integer getAmount() {
-            return 500;
-        }
-
-        @Override
-        public LocalDate getDate() {
-            return LocalDate.of(2021, 1, 14);
-        }
-
-        @Override
-        public Integer getBalance() {
-            return 500;
-        }
-    }
-
-    protected class MockTransactionCreater implements TransactionCreater {
-        private int amount;
-        private LocalDate date;
-        private int balance;
-
-        @Override
-        public MockTransaction create(int amount, LocalDate date, int currentBalance) {
-            this.amount = amount;
-            this.date = date;
-            this.balance = currentBalance;
-
-            return new MockTransaction();
-        }
-
-        public int getAmount() {
-            return amount;
-        }
-
-        public LocalDate getDate() {
-            return date;
-        }
-
-        public int getBalance() {
-            return balance;
-        }
-    }
-
-    private MockTransactionCreater mockTransactionCreater;
+    private BankTransactionCreater mockTransactionCreater;
     private BankStatementPrinter mockStatementPrinter;
     private BankAccount bankAccount;
+    private BankTransaction mockTransaction;
 
     @BeforeEach
     private void setUp(){
-        mockTransactionCreater = new MockTransactionCreater();
+        mockTransactionCreater = Mockito.mock(BankTransactionCreater.class);
         mockStatementPrinter = Mockito.mock(BankStatementPrinter.class);
         bankAccount = new BankAccount(mockTransactionCreater, mockStatementPrinter);
+        mockTransaction = Mockito.mock(BankTransaction.class);
+        Mockito.when(mockTransactionCreater.create(anyInt(), any(LocalDate.class), anyInt())).thenReturn(mockTransaction);
+        Mockito.when(mockTransaction.getBalance()).thenReturn(500);
     }
 
     @Test
-    public void withdrawCreatesANewTransaction() {
+    public void withdrawCallsTransactionCreater() {
         bankAccount.withdraw(500, LocalDate.of(2021, 1, 14));
-        assertThat(mockTransactionCreater.getAmount(), equalTo(500) );
-        assertThat(mockTransactionCreater.getDate(), equalTo(LocalDate.of(2021, 1, 14)) );
-        assertThat(mockTransactionCreater.getBalance(), equalTo(-500) );
+        Mockito.verify(mockTransactionCreater).create(500,LocalDate.of(2021, 1, 14 ), -500);
     }
 
     @Test
-    public void depositCreatesANewTransaction() {
+    public void depositCallsTransactionCreater() {
         bankAccount.deposit(500, LocalDate.of(2021, 2, 15));
-        assertThat(mockTransactionCreater.getAmount(), equalTo(500) );
-        assertThat(mockTransactionCreater.getDate(), equalTo(LocalDate.of(2021, 2, 15)) );
-        assertThat(mockTransactionCreater.getBalance(), equalTo(500) );
+        Mockito.verify(mockTransactionCreater).create(500,LocalDate.of(2021, 2, 15 ), 500);
     }
 
     @Test
@@ -88,25 +41,19 @@ public class BankAccountTestSuite {
         for(int i=0; i<2; i++) {
             bankAccount.deposit(500, LocalDate.of(2021, 2, 15));
         }
-        assertThat(mockTransactionCreater.getBalance(), equalTo(1000) );
+        Mockito.verify(mockTransactionCreater).create(anyInt(), any(LocalDate.class), eq(1000));
     }
 
     @Test
     public void withdrawCreatesANewTransactionWithTheRightBalance() {
-        bankAccount.withdraw(500, LocalDate.of(2021, 2, 15));
-        bankAccount.withdraw(500, LocalDate.of(2021, 2, 15));
-        assertThat(mockTransactionCreater.getBalance(), equalTo(0) );
+        for(int i=0; i<2; i++) {
+            bankAccount.withdraw(500, LocalDate.of(2021, 2, 15));
+        }
+        Mockito.verify(mockTransactionCreater).create(anyInt(), any(LocalDate.class), eq(0));
     }
 
     @Test
     public void generateStatementCallsStatementPrinter() {
-        BankTransactionCreater mockTransactionCreater = Mockito.mock(BankTransactionCreater.class);
-        BankStatementPrinter mockStatementPrinter = Mockito.mock(BankStatementPrinter.class);
-        BankAccount bankAccount = new BankAccount(mockTransactionCreater, mockStatementPrinter);
-
-        BankTransaction mockTransaction = Mockito.mock(BankTransaction.class);
-        Mockito.when(mockTransactionCreater.create(anyInt(), any(LocalDate.class), anyInt())).thenReturn(mockTransaction);
-
         bankAccount.withdraw(500, LocalDate.of(2021, 2, 15));
         bankAccount.withdraw(500, LocalDate.of(2021, 2, 15));
         bankAccount.withdraw(500, LocalDate.of(2021, 2, 15));
